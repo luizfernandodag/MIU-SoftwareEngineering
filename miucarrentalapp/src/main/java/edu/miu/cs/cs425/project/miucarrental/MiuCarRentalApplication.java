e7e800688cb53aa26d36b3af8048366375351a99
edenBr81 <37551602+edenBr81@users.noreply.github.com>
2020-06-10 11:25:25 -0500

    Add files via upload

---
+++ miucarrentalapp/src/main/java/edu/miu/cs/cs425/project/miucarrental/MiuCarRentalApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.project.miucarrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiuCarRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiuCarRentalApplication.class, args);
+    }
+
+}
---
+++ miucarrentalapp/src/test/java/edu/miu/cs/cs425/project/miucarrental/MiuCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.project.miucarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiuCarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
