ea1b67d0c14cacdd3f0372011018c4ed39bdd1f4
luizfernandodag <luizfernandodag@gmail.com>
2020-06-15 02:27:17 -0300

    bug correction

--- miucarrentalapp/src/main/java/edu.miu.cs.cs425.project.miucarrental.controller/RentalCarController.java
+++ miucarrentalapp/src/main/java/edu.miu.cs.cs425.project.miucarrental.controller/RentalCarController.java
@@ -18,84 +18,84 @@ import java.util.List;
 
 @Controller
 public class RentalCarController{
-    @Autowired
-    private RentalCarService rentalCarService;
-    public RentalCarController(RentalCarService RentalCarService) {
-        this.rentalCarService = RentalCarService;
-    }
-
-    @GetMapping(value = {"/miucarrent/RentalCar/list", "/RentalCar/list"})
-    public ModelAndView listRentalCar() {
-        ModelAndView modelAndView = new ModelAndView();
-
-        List<RentalCar> RentalCars = rentalCarService.findAll();
-        modelAndView.addObject("RentalCars", RentalCars);
-        modelAndView.addObject("searchString", "");
-        modelAndView.addObject("RentalCarsCount", RentalCars.size());
-        modelAndView.setViewName("RentalCar/list");
-        return modelAndView;
-
-    }
-
-    @GetMapping(value = {"/miucarrent/RentalCar/new","/RentalCar/new"})
-    public String displayNewRentalCarForm(Model model) {
-        model.addAttribute("RentalCar", new RentalCar());
-        return "RentalCar/new";
-    }
-
-
-    @PostMapping(value = {"/miucarrent/RentalCar/new","/RentalCar/new"})
-    public String addNewRentalCar(@Valid @ModelAttribute("RentalCar") RentalCar rentalcar,
-                             BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("errors", bindingResult.getAllErrors());
-            return "RentalCar/new";
-        }
-        rentalcar = rentalCarService.save(rentalcar);
-        return "redirect:/miucarrent/RentalCar/list";
-    }
-
-    @GetMapping(value = {"/miucarrent/RentalCar/edit/{RentalCarId}","/RentalCar/edit/{RentalCarId}"})
-    public String editRentalCar(@PathVariable Integer RentalCarId, Model model) {
-        RentalCar RentalCar = rentalCarService.findById(RentalCarId);
-
-        if (RentalCar != null) {
-            model.addAttribute("RentalCar", RentalCar);
-            return "RentalCar/edit";
-        }
-        return "RentalCar/list";
-    }
-
-    @PostMapping(value = {"/miucarrent/RentalCar/edit","/RentalCar/edit"})
-    public String updateRentalCar(@Valid @ModelAttribute("RentalCar") RentalCar RentalCar,
-                             BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("errors", bindingResult.getAllErrors());
-            return "RentalCar/edit";
-        }
-
-        return "redirect:/miucarrent/RentalCar/list";
-    }
-
-    @GetMapping(value = {"/miucarrent/RentalCar/delete/{RentalCarId}","/RentalCar/delete/{RentalCarId}"})
-    public String deleteRentalCar(@PathVariable Integer RentalCarId, Model model) {
-        rentalCarService.deleteById(RentalCarId);
-        return "redirect:/miucarrent/RentalCar/list";
-
-    }
-
-    @GetMapping(value = {"/miucarrent/RentalCar/search", "/RentalCar/search"})
-    public ModelAndView searchRentalCars(@RequestParam String searchString) {
-        ModelAndView modelAndView = new ModelAndView();
-
-        List<RentalCar> RentalCars = rentalCarService.searchRentalCars(searchString);
-
-        modelAndView.addObject("RentalCars", RentalCars);
-        modelAndView.addObject("searchString", searchString);
-        modelAndView.addObject("RentalCarsCount", RentalCars.size());
-        modelAndView.setViewName("RentalCar/list");
-        return modelAndView;
-    }
+//    @Autowired
+//    private RentalCarService rentalCarService;
+//    public RentalCarController(RentalCarService RentalCarService) {
+//        this.rentalCarService = RentalCarService;
+//    }
+//
+//    @GetMapping(value = {"/miucarrent/RentalCar/list", "/RentalCar/list"})
+//    public ModelAndView listRentalCar() {
+//        ModelAndView modelAndView = new ModelAndView();
+//
+//        List<RentalCar> RentalCars = rentalCarService.findAll();
+//        modelAndView.addObject("RentalCars", RentalCars);
+//        modelAndView.addObject("searchString", "");
+//        modelAndView.addObject("RentalCarsCount", RentalCars.size());
+//        modelAndView.setViewName("RentalCar/list");
+//        return modelAndView;
+//
+//    }
+//
+//    @GetMapping(value = {"/miucarrent/RentalCar/new","/RentalCar/new"})
+//    public String displayNewRentalCarForm(Model model) {
+//        model.addAttribute("RentalCar", new RentalCar());
+//        return "RentalCar/new";
+//    }
+//
+//
+//    @PostMapping(value = {"/miucarrent/RentalCar/new","/RentalCar/new"})
+//    public String addNewRentalCar(@Valid @ModelAttribute("RentalCar") RentalCar rentalcar,
+//                             BindingResult bindingResult, Model model) {
+//        if (bindingResult.hasErrors()) {
+//            model.addAttribute("errors", bindingResult.getAllErrors());
+//            return "RentalCar/new";
+//        }
+//        rentalcar = rentalCarService.save(rentalcar);
+//        return "redirect:/miucarrent/RentalCar/list";
+//    }
+//
+//    @GetMapping(value = {"/miucarrent/RentalCar/edit/{RentalCarId}","/RentalCar/edit/{RentalCarId}"})
+//    public String editRentalCar(@PathVariable Integer RentalCarId, Model model) {
+//        RentalCar RentalCar = rentalCarService.findById(RentalCarId);
+//
+//        if (RentalCar != null) {
+//            model.addAttribute("RentalCar", RentalCar);
+//            return "RentalCar/edit";
+//        }
+//        return "RentalCar/list";
+//    }
+//
+//    @PostMapping(value = {"/miucarrent/RentalCar/edit","/RentalCar/edit"})
+//    public String updateRentalCar(@Valid @ModelAttribute("RentalCar") RentalCar RentalCar,
+//                             BindingResult bindingResult, Model model) {
+//        if (bindingResult.hasErrors()) {
+//            model.addAttribute("errors", bindingResult.getAllErrors());
+//            return "RentalCar/edit";
+//        }
+//
+//        return "redirect:/miucarrent/RentalCar/list";
+//    }
+//
+//    @GetMapping(value = {"/miucarrent/RentalCar/delete/{RentalCarId}","/RentalCar/delete/{RentalCarId}"})
+//    public String deleteRentalCar(@PathVariable Integer RentalCarId, Model model) {
+//        rentalCarService.deleteById(RentalCarId);
+//        return "redirect:/miucarrent/RentalCar/list";
+//
+//    }
+//
+//    @GetMapping(value = {"/miucarrent/RentalCar/search", "/RentalCar/search"})
+//    public ModelAndView searchRentalCars(@RequestParam String searchString) {
+//        ModelAndView modelAndView = new ModelAndView();
+//
+//        List<RentalCar> RentalCars = rentalCarService.searchRentalCars(searchString);
+//
+//        modelAndView.addObject("RentalCars", RentalCars);
+//        modelAndView.addObject("searchString", searchString);
+//        modelAndView.addObject("RentalCarsCount", RentalCars.size());
+//        modelAndView.setViewName("RentalCar/list");
+//        return modelAndView;
+//    }
 
 
 }
